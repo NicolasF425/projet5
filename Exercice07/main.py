95174fb7902862d57898461f30d385b021d07c03
NicolasF425 <nifan42@gmail.com>
2025-09-08 16:41:37 +0200

    ajout code pour les exercices

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,7 @@
-## Écrivez votre code ici !
+def square(value):
+    try:
+        square_value = value * value
+        return square_value
+    except ValueError:
+        print("Le paramètre doit être un nombre !")
+        return None
--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,15 @@
 def log_decorator(func):
-     pass
- 
+    def wrapper():
+        print("Avant la fonction\n")
+        result = func()
+        print("\nAprès la fonction")
+        return result
+    return wrapper
+
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
+
 
 function_test()
